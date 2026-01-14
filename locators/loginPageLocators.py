18ea4087f45befc0597565a12b0b4a808e91bd6e
saikiran <saikiran@example.com>
2026-01-14 18:35:29 +0530

    Delete the values

--- locators/loginPageLocators.py
+++
@@ -1,7 +0,0 @@
-class LoginPageLocators:
-    ADMIN_BUTTON = "//*[@id='root']/header/div/div/a[2]"
-    USERNAME_FIELD = '//*[@id=":r7:"]'
-    PASSWORD_FIELD = '//*[@id=":r9:"]'
-    SUBMIT_BUTTON = '//*[@id="auth-tabpanel-0"]/div/form/button'
-    LOGOUT = "//*[@id='root']/header/div/div/button[1]"
-    INVALID_CREDENTIALS_MESSAGE = "//*[contains(text(),'Invalid credentials')]"
